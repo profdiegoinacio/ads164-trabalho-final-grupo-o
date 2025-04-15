b760a3e3954837e3ecfbe2bada0c2c62ff848e16
joaovtfor <joaovtfor@hotmail.com>
2025-04-15 09:32:56 -0300

    `Feat` - Base project themes

--- app/src/main/java/br/com/listme/ui/theme/Color.kt
+++ app/src/main/java/br/com/listme/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package br.com.listme.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DefaultBlue = Color(0XFF37527C)
+val LightBlue = Color(0xFFB1CFFF)
+val DarkBlue = Color(0xFF323E52)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Yellow = Color(0xFFBCA35F)
+val Red = Color(0xFF9D1D18)
+val DarkRed = Color(0xFF742725)
+val Green = Color(0xFF189E46)
+val DarkGreen = Color(0xFF234930)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Background = Color(0xFFEEEEEE)
